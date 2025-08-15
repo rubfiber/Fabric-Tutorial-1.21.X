a1d630d3abf086312d0bbb012664ed96bbebd7e8
js12488 <broporg@gmail.com>
2025-08-14 21:26:36 -0400

    Made some changes to TutorialMod

--- src/main/java/net/rubfiber/tutorialmod/TutorialMod.java
+++ src/main/java/net/rubfiber/tutorialmod/TutorialMod.java
@@ -2,15 +2,21 @@ package net.rubfiber.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.rubfiber.tutorialmod.block.ModBlocks;
+import net.rubfiber.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.rubfiber.tutorialmod.PlaceBlockClass;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems(); //register item, blocks, etc
+		ModBlocks.registerModBlocks();
+		PlaceBlockClass.placeBlockAtPlayerLocation();
+
 
 	}
 }
